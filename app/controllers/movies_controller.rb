b9e9c69cb65dbb5751f63d400bf1e4dae71db9ff
jukebox101 <leonora.squires@gmail.com>
2020-06-13 16:16:01 -0400

    created index route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,17 @@
 class MoviesController < ApplicationController
+    def index
+        @movies = Movie.all
+    end
+
+    def show
+
+    end
+
+    def new
+
+    end
+
+    def create
+
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
-    
-end
+
+    end
