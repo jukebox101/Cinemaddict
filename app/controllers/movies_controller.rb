f3e92b853dc9e1f517d9ae017e9d6a574a5bc331
jukebox101 <leonora.squires@gmail.com>
2020-06-13 17:03:55 -0400

    create new route

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,10 +8,28 @@ class MoviesController < ApplicationController
     end
 
     def new
-
+        @movie = Movie.new
     end
 
     def create
+        movie_params = params.require(:movie).permit(:title, :genre, :description, :rating)
+        @movie = Movie.create(movie_params)
 
+        redirect_to movie_path(@movie)
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        movie = Movie.find(params[:id])
+        movie_params = params.require(:movie).permit(:title, :genre, :description, :rating)
+        movie.update(movie_params)
+
+        redirect_to movie_path(movie)
+    end
+
+    def destroy
     end
 end
