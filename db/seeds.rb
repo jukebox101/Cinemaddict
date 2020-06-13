69981505f43fe25fff8b8b5f0a3ba8c56b556e7f
j3ffh95 <j3ffh95@gmail.com>
2020-06-13 15:29:04 -0400

    Added more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,5 +11,16 @@ Review.destroy_all
 Movie.destroy_all
 
 user1 = User.create(username: "jeff", first_name: "Jefferson", last_name: "Huisa", password: "test", email: "jeff@gmail.com")
+user2 = User.create(username: "Lilly", first_name: "Leonora", last_name: "Squires", password: "test1", email: "lilly@gmail.com")
+user3 = User.create(username: "johnD", first_name: "John", last_name: "Doe", password: "test2", email: "johnD@gmail.com")
+
 movie1 = Movie.create(title: "Avatar", genre: "Action", description: "Cool blue people", rating: "PG-13")
+movie2 = Movie.create(title: "The Revenant", genre: "Drama", description: "sustains life-threatening injuries from a brutal bear attack.", rating: "R")
+movie3 = Movie.create(title: "The Hangover", genre: "Comedy", description: "All three films follow the misadventures", rating: "R")
+movie4 = Movie.create(title: "The Terminator", genre: "Action", description: "Skynet goes live", rating: "R")
+movie5 = Movie.create(title: "Toy Story 1", genre: "Action", description: "Story about toys", rating: "G")
+
 review1 = Review.create(description: "Great movie", user_id: user1.id, movie_id: movie1.id, time_posted: Time.now)
+review2 = Review.create(description: "Not a good movie", user_id: user3.id, movie_id: movie3.id, time_posted: Time.now)
+review3 = Review.create(description: "Watching again", user_id: user3.id, movie_id: movie2.id, time_posted: Time.now)
+review4 = Review.create(description: "Awesome movie", user_id: user2.id, movie_id: movie5.id, time_posted: Time.now)
