7571484d3643e62a8c937a6c93b91d869359f794
j3ffh95 <j3ffh95@gmail.com>
2020-06-14 12:26:58 -0400

    modified reviews controller and added reviews views

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,46 @@
 class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    review = Review.create(review_params)
+    # review.time_posted = Time.now
+    # byebug
+
+    redirect_to reviews_path
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    review = Review.find(params[:id])
+    review.update(review_params)
+
+    redirect_to review_path(review)
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :user_id, :movie_id, :date_posted)
+  end
+
 end
